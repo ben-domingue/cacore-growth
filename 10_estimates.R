3a8c33d45c0f2ec0cbfb85d5dd8fb1c8d17f0096
Ben Domingue <ben.domingue@gmail.com>
2025-12-08 17:55:35 -0800

    ...

--- 10_estimates.R
+++ 10_estimates.R
@@ -4,72 +4,38 @@
 ################################################################
 ## School-grade growth estimates with related uncertainty information. 
 load("_estimates.Rdata")
-library(weights)
-grest<-list() ##get growth estimates `grest`
-for (i in 1:length(estimates)) {
-    z1<-estimates[[i]]$co$coef
-    z2<-estimates[[i]]$co.grade$coef
-    grest[[names(estimates)[i] ]]<-list(co=z1,co.grade=z2)
+##
+f<-function(x) {
+    z<-lapply(x,function(x) x$coef)
+    z<-do.call("rbind",z)
+    z<-data.frame(z)
+    names(z)<-paste("ela_",names(z),sep='')
+    txt<-strsplit(rownames(z),"schoolid_",fixed=TRUE)
+    txt<-sapply(txt,function(x) x[2])
+    z$id<-sapply(strsplit(txt,"__",fixed=TRUE),function(x) x[1])
+    z$grade<-sapply(strsplit(txt,"__",fixed=TRUE),function(x) x[2])
+    z
 }
-
-L<-lapply(grest,function(x) x$co.grade)
-for (i in 1:length(L)) L[[i]]$subject<-names(L)[i]
-x<-data.frame(do.call("rbind",L))
-
-nms<-names(x)
-x$id<-rownames(x)
-rownames(x)<-NULL
-x<-x[,c("id",nms)]
-
-z<-strsplit(x$id,"schoolid_",fixed=TRUE)
-z<-sapply(z,function(x) x[2])
-z<-strsplit(z,"__",fixed=TRUE)
-id<-sapply(z,function(x) x[1])
-gr<-sapply(z,function(x) x[2])
-gr<-ifelse(is.na(gr),11,gr)
-x$school<-id
-x$grade<-gr
+L<-lapply(estimates,f)
+names(L[[2]])<-gsub("ela_","math_",names(L[[2]]),fixed=TRUE)
+x<-merge(L[[1]],L[[2]],all=TRUE)
 
 write.csv(x,file="/tmp/est.csv",quote=FALSE,row.names=FALSE)
 
 ################################################################
 ## School growth estimates with related uncertainty information. 
-load("_estimates.Rdata")
-library(weights)
-grest<-list() ##get growth estimates `grest`
-for (i in 1:length(estimates)) {
-    z1<-estimates[[i]]$co$coef
-    z2<-estimates[[i]]$co.grade$coef
-    grest[[names(estimates)[i] ]]<-list(co=z1,co.grade=z2)
-}
-##aggregate to school-level
-z1<-grest[[1]]$co.grade
-names(z1)<-paste(names(z1),names(grest)[1],sep='')
-z2<-grest[[2]]$co.grade
-names(z2)<-paste(names(z2),names(grest)[2],sep='')
-z<-merge(z1,z2,by=0,all=TRUE)
-txt<-strsplit(z[,1],"_")
-z$id<-sapply(txt,function(x) x[2])
-L<-split(z,z$id)
-f<-function(x) {
-    m1<-Hmisc::wtd.mean(x$fe_ela,x$n_ela)
-    m2<-Hmisc::wtd.mean(x$eb_ela,x$n_ela)
-    m3<-Hmisc::wtd.mean(x$fe_math,x$n_math)
-    m4<-Hmisc::wtd.mean(x$eb_math,x$n_math)
-    se1<-sqrt(sum(x$eb.se_ela^2))
-    se2<-sqrt(sum(x$eb.se_math^2))
-    c(fe.ela=m1,eb.ela=m2,eb.ela.se=se1,fe.math=m3,eb.math=m4,eb.math.se=se2)
-}
-x<-t(sapply(L,f))
-x<-data.frame(id=rownames(x),x)
-write.csv(x,file="/tmp/school.csv",quote=FALSE,row.names=FALSE)
+load("_sch.Rdata")
+write.csv(sch,file="/tmp/school.csv",quote=FALSE,row.names=FALSE)
 
 ################################################################
 ## Student residuals that were used in computation of (I). 
 load("_estimates.Rdata")
-r1<-estimates[[1]]$co.grade$m3$m3resid
-r2<-estimates[[2]]$co.grade$m3$m3resid
+f<-function(x) x$m3$m3resid[,c("id","m3resid")]
+r1<-lapply(estimates[[1]],f)
+r1<-data.frame(do.call("rbind",r1))
 names(r1)[2]<-"_ela"
+r2<-lapply(estimates[[2]],f)
+r2<-data.frame(do.call("rbind",r2))
 names(r2)[2]<-"_math"
 r<-merge(r1,r2)
 write.csv(r,file="/tmp/residual.csv",quote=FALSE,row.names=FALSE)
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - 00_data.R preparing the data for analysis
 - 01_descriptives.R descriptive statistics
 - 02_model.R running basic growth model
+- 02b_combine_estimates.R combine the school-grade estimates into school estimates
 - 03_analysis.R analysis of the growth estimates
 - 04_stability.R association of 24 and 25 estimates
 - 05_compare.R compare to EA 24 results
@@ -11,10 +12,7 @@
 
 ## issues
 
-- get updated info from belen
-
+- produce estimates
 - need to do analysis for each subgroup
 
-## notes re 2024 results
-- bunch of schools with very different sample sizes (they are basically 0 in 2025 but several hundred year prior). they don't drive differences in growth estimates. 101337
 
